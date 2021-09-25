eec77c012f0720b984c655752c3f1c6143806ffd
juliaschmidt0710 <julia.luisa.schmidg@gmail.com>
2021-09-25 15:56:34 -0300

    commit

---
+++ App.Persistence/Migrations/20210925114616_Update6.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Persistence.Migrations
+{
+    public partial class Update6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cpf",
+                table: "pessoa",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rg",
+                table: "pessoa",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Telefone",
+                table: "pessoa",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cpf",
+                table: "pessoa");
+
+            migrationBuilder.DropColumn(
+                name: "Rg",
+                table: "pessoa");
+
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "pessoa");
+        }
+    }
+}
